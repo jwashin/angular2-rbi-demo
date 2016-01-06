d4382f5d23ddc578dabf45a5ce4391bd023eaf16
Kasper Peulen <kasperpeulen@gmail.com>
2016-01-06 01:37:13 +0100

    put all dart files in lib, except main

--- web/main.dart
+++ web/main.dart
@@ -3,7 +3,7 @@ import 'package:angular2/core.dart';
 import 'package:angular2/router.dart';
 import 'package:contact_list/contacts.dart';
 
-import 'app.dart';
+import 'package:contact_list/app.dart';
 
 main() => bootstrap(App, [
       Contacts,

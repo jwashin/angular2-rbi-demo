cda8ae3d6a288e62a22a8b88b0286700b8445333
Kasper Peulen <kasperpeulen@gmail.com>
2016-01-06 02:38:02 +0100

    rename textfields to textFields

--- lib/components/edit_contact.dart
+++ lib/components/edit_contact.dart
@@ -38,8 +38,8 @@ class EditContact implements AfterContentChecked {
     // don't appear to know their values.
 
     if (uuid != '') {
-      List<Element> textfields = querySelectorAll('.mdl-js-textfield');
-      for (Element k in textfields) {
+      List<Element> textFields = querySelectorAll('.mdl-js-textfield');
+      for (Element k in textFields) {
         if (k.firstChild.text != null && k.firstChild.text.length > 0) {
           k.classes.add(IS_DIRTY);
         }

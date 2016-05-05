73adeb3e021a126a9cbdd9e15591edc4b64eea59
Jim Washington <washington.jim@gmail.com>
2016-05-05 07:25:16 -0400

    update; to work with angular-2.0.0-beta.17

--- web/main.dart
+++ web/main.dart
@@ -1,6 +1,7 @@
-import 'package:angular2/platform/browser.dart';
 import 'package:angular2/core.dart';
 import 'package:angular2/router.dart';
+import 'package:angular2/platform/common.dart';
+import 'package:angular2/platform/browser.dart';
 
 //import 'package:uuid/uuid.dart';
 import 'package:contact_list/services/contacts.dart';
